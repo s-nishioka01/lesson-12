a7f9b852ad44d4b111161e4e20df201b933abeb5
s.n <s.n.@sns-MacBook-Air-2020.local>
2022-07-26 17:48:16 +0900

    GetMappingを修正

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
@@ -27,8 +26,8 @@ public class UserController {
 		this.userService = userService;
 	}
 
-	@GetMapping("/users")
-	public ResponseEntity<User> getUser(@RequestParam(name = "id") int id) throws Exception {
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUser(@PathVariable("id") int id) throws Exception {
 		return ResponseEntity.ok(userService.findOne(id));
 	}
 
